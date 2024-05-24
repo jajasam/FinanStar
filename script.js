ef13b36b07b6a831d4618c00d8d396157f51f100
VeloomETS <jana.samson@hotmail.fr>
2024-05-24 10:40:49 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+import './scripts/navLinks.js';
+import './scripts/projects.js';
+import './scripts/scrollingAnimations.js';
+import './scripts/scrollingEvents.js';
+import './scripts/skills.js';
+import './scripts/menu.js';
+import './scripts/socials.js';
+import './scripts/lang.js';
+import './scripts/theme.js';
